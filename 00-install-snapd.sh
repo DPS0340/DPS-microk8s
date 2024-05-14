fab7e206db9faa08e6441e71a242b5718d51c729
Jiho Lee <optional.int@kakao.com>
2024-05-14 23:31:27 +0900

    feat: Initial commit

---
+++ 00-install-snapd.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Enable debug output
+PS4="\n\033[1;33m>>\033[0m "; set -x
+
+LOCATION=$(realpath "$0")
+DIR=$(dirname "$LOCATION")
+
+git clone https://aur.archlinux.org/snapd.git
+
+cd $DIR/snapd
+
+# For prevent seccomp.h dependency missing on nix-env gcc
+CC=/usr/bin/gcc makepkg -si
+cd ..
+
+sudo systemctl enable --now snapd
